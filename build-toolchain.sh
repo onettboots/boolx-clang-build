b6a758eb056c0b546a2bf01c48e0816c749932a9
RuRuTiaSaMa <1009087450@qq.com>
2022-02-01 22:56:36 +0800

    build-toolchain.sh: Rename to Maou Clang
    Signed-off-by: RuRuTiaSaMa <1009087450@qq.com>

--- build-toolchain.sh
+++ build-toolchain.sh
@@ -13,7 +13,7 @@ function msg() {
 # Build LLVM
 msg "Building LLVM..."
 ./build-llvm.py \
-	--clang-vendor "Proton" \
+	--clang-vendor "Maou" \
 	--targets "ARM;AArch64;X86" \
 	"$repo_flag" \
 	--pgo kernel-defconfig \
