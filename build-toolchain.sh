9318f4734d38f5142962062f738438b2405be92e
KyuoFoxHuyu <88403766@qq.com>
2022-02-09 12:54:14 +0800

    build-tc.sh: Build bolt
    Signed-off-by: KyuoFoxHuyu <88403766@qq.com>
    Signed-off-by: RuRuTiaSaMa <1009087450@qq.com>

--- build-toolchain.sh
+++ build-toolchain.sh
@@ -17,6 +17,7 @@ msg "Building LLVM..."
 	--targets "ARM;AArch64;X86" \
 	--defines "LLVM_PARALLEL_COMPILE_JOBS=$(nproc) LLVM_PARALLEL_LINK_JOBS=$(nproc) CMAKE_C_FLAGS=-O3 CMAKE_CXX_FLAGS=-O3 LLVM_USE_LINKER=lld LLVM_ENABLE_LLD=ON" \
 	"$repo_flag" \
+	--projects "clang;lld;polly;compiler-rt;bolt" \
 	--pgo kernel-defconfig \
 	--lto thin
 
