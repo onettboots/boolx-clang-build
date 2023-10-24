885aa41c9bebd3c1e7dd26c6bd2613c1258665b5
Nathan Chancellor <nathan@kernel.org>
2023-10-24 12:05:53 -0500

    Merge pull request #247 from nathanchance/build-targets
    build-llvm.py: Add '--build-targets'
    
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- tc_build/llvm.py
+++ tc_build/llvm.py
@@ -27,6 +27,7 @@ class LLVMBuilder(Builder):
 
         self.bolt = False
         self.bolt_builder = None
+        self.build_targets = ['all']
         self.ccache = False
         self.check_targets = []
         # Removes system dependency on terminfo to keep the dynamic library
@@ -142,7 +143,7 @@ class LLVMBuilder(Builder):
             raise RuntimeError('BOLT requested without a builder?')
 
         build_start = time.time()
-        ninja_cmd = ['ninja', '-C', self.folders.build]
+        ninja_cmd = ['ninja', '-C', self.folders.build, *self.build_targets]
         self.run_cmd(ninja_cmd)
 
         if self.check_targets:
