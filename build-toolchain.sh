7ed6b1c2d7bdc820d77850e5eb291a64c1fd9aff
onettboots <blackcocopet@gmail.com>
2024-10-26 14:35:30 +0000

    build clang 20-init
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- build-toolchain.sh
+++ build-toolchain.sh
@@ -15,7 +15,7 @@ msg "Building LLVM..."
 ./build-llvm.py \
 	--clang-vendor "Bool-X" \
 	--targets "ARM;AArch64;X86" \
-	--branch "llvmorg-17-init" \
+	--branch "llvmorg-20-init" \
 	--defines "LLVM_PARALLEL_COMPILE_JOBS=$(nproc) LLVM_PARALLEL_LINK_JOBS=$(nproc) CMAKE_C_FLAGS=-O3 CMAKE_CXX_FLAGS=-O3 LLVM_USE_LINKER=lld LLVM_ENABLE_LLD=ON" \
 	"$repo_flag" \
 	--projects "clang;lld;polly;compiler-rt;bolt" \
